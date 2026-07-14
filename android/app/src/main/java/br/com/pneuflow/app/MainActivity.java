9b21fb2993d31c9bb03d0c64721dc72ca2544e1d
Pedro Salomão Camilo <pedrosalomao22099@gmail.com>
2026-07-14 00:42:13 -0300

    feat(android): add PneuFlow branding and native visual polish (#43)

--- android/app/src/main/java/br/com/pneuflow/app/MainActivity.java
+++ android/app/src/main/java/br/com/pneuflow/app/MainActivity.java
@@ -1,5 +1,14 @@
 package br.com.pneuflow.app;
 
+import android.graphics.Color;
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getBridge().getWebView().setBackgroundColor(Color.rgb(10, 11, 16));
+    }
+}
